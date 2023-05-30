0df5e088449523b4056af92c69288a3a6ba814f7
AhmadElHalabi10 <ahmd.el.halabi.4@gmail.com>
2023-05-30 19:19:05 +0300

    Initial commit

---
+++ src/pages/admin/dashboard/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./dashboard.css";
+import SideBar from "/home/ahmad/Desktop/Final-Project/Brokerage-Repository/Brokerage-FinalProject-FrontEnd/almorad/src/components/sidebar";
+import { Outlet } from "react-router-dom";
+
+export default function Dashboard() {
+  return (
+    <div className="dashboard">
+      <SideBar />
+      <Outlet />
+      {/* <div className="dashboard-container">Dashboard</div> */}
+    </div>
+  );
+}
---
+++ src/pages/admin/unauthorized/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./unauthorized.css";
+import MainButton from "/home/ahmad/Desktop/Final-Project/Brokerage-Repository/Brokerage-FinalProject-FrontEnd/almorad/src/components/mainbutton";
+import { Link } from "react-router-dom";
+
+export default function Unauthorized() {
+  return (
+    <div className="unauthorized-page">
+      <div className="unauthorized-page-container">
+        <h2>unauthorized</h2>
+        <div>401 - You don't have access to this page</div>
+        <div>This page is not publicly available.</div>
+        <div>To access this page please login first.</div>
+        <Link to="/admin-login">
+          <MainButton name="Login" />
+        </Link>
+      </div>
+    </div>
+  );
+}
