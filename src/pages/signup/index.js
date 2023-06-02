5cd1792400630f65948cc448ae10ef71f116833b
AhmadElHalabi10 <ahmd.el.halabi.4@gmail.com>
2023-06-02 16:20:00 +0300

    Fixing the signup functionality

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./signup.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
 export default function SignUp() {
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  // ...other code...
+  // const [userName, setUserName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [submitError, setSubmitError] = useState(null);
   const [submitSuccess, setSubmitSuccess] = useState(false);
 
-  // const navigate = useNavigate();
-
-  // const handleGoBack = () => {
-  //   navigate("/");
-  // };
+  // const [showPassword, setShowPassword] = useState(false);
+  const [formValues, setFormValues] = useState({
+    userName: "",
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+  });
+  const [errors, setErrors] = useState({
+    userName: "",
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+  });
+  const navigate = useNavigate();
 
   const goBack = () => {
     window.history.back();
@@ -32,35 +44,89 @@ export default function SignUp() {
     setSubmitSuccess(false);
     setIsSubmitting(true);
 
-    try {
-      // Make the Axios POST request
-      const response = await axios.post("http://localhost:7000/auth/register", {
-        userName,
-        email,
-        password,
-        firstName,
-        lastName,
-      });
+    // Perform validation logic here
+    let hasErrors = false;
 
-      // Handle the response if needed
-      console.log(response.data);
-
-      // Set success message and reset form
-      setSubmitSuccess(true);
-      setUserName("");
-      setEmail("");
-      setPassword("");
-      setFirstName("");
-      setLastName("");
-    } catch (error) {
-      // Handle the error if needed
-      console.error(error);
-      setSubmitError("An error occurred. Please try again.");
-    } finally {
-      setIsSubmitting(false);
+    if (formValues.userName === "") {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        userName: "Please enter a user name.",
+      }));
+      hasErrors = true;
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, userName: "" }));
     }
-  };
 
+    if (formValues.email === "") {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "Please enter an email.",
+      }));
+      hasErrors = true;
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, email: "" }));
+    }
+
+    if (formValues.password === "") {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        password: "Please enter a password.",
+      }));
+      hasErrors = true;
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, password: "" }));
+    }
+
+    if (formValues.firstName === "") {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        firstName: "Please enter a first name.",
+      }));
+      hasErrors = true;
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, firstName: "" }));
+    }
+
+    if (hasErrors) {
+      return;
+    }
+
+    // Submit form if validation passes
+    const formData = {
+      userName: formValues.userName,
+      email: formValues.email,
+      password: formValues.password,
+      firstName: formValues.firstName,
+      lastName: formValues.lastName,
+    };
+
+    // Make the Axios POST request
+    axios
+      .post("https://almorad-app-api.onrender.com/auth/register", formData)
+      .then((response) => {
+        console.log(response);
+        navigate("/login");
+      })
+      .catch((err) => {
+        if (err.response && err.response.data && err.response.data.errors) {
+          const { email, firstName, lastName } = err.response.data.errors;
+          setErrors((prevErrors) => ({
+            ...prevErrors,
+            email: email ? "Email already exists." : "",
+            firstName: firstName ? "First name already exists." : "",
+            lastName: lastName ? "Last name already exists." : "",
+            general: "", // Reset general error message if present
+          }));
+        } else {
+          setErrors((prevErrors) => ({
+            ...prevErrors,
+            general:
+              "An error occurred. Please try to change email or first name or last name", // Set a general error message
+          }));
+          console.log(err);
+        }
+      });
+  };
   return (
     <div className="signup">
       <div className="signup-signupbox">
@@ -71,38 +137,48 @@ export default function SignUp() {
           <h1 className="signup-signupbox-title">Signup</h1>
           <input
             type="text"
-            placeholder="Username"
+            placeholder="UserName"
             className="signup-signupbox-input"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
+            value={formValues.userName}
+            onChange={(event) =>
+              setFormValues({ ...formValues, userName: event.target.value })
+            }
           />
           <input
             type="email"
             placeholder="Email"
             className="signup-signupbox-input"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={formValues.email}
+            onChange={(event) =>
+              setFormValues({ ...formValues, email: event.target.value })
+            }
           />
           <input
             type="password"
             placeholder="Password"
             className="signup-signupbox-input"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={formValues.password}
+            onChange={(event) =>
+              setFormValues({ ...formValues, password: event.target.value })
+            }
           />
           <input
             type="text"
             placeholder="First Name"
             className="signup-signupbox-input"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
+            value={formValues.firstName}
+            onChange={(event) =>
+              setFormValues({ ...formValues, firstName: event.target.value })
+            }
           />
           <input
             type="text"
             placeholder="Last Name"
             className="signup-signupbox-input"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
+            value={formValues.lastName}
+            onChange={(event) =>
+              setFormValues({ ...formValues, lastName: event.target.value })
+            }
           />
           {submitError && <p className="signup-error-message">{submitError}</p>}
           {submitSuccess && (
