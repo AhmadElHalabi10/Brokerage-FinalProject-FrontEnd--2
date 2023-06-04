8996785a9481d7f5255a7d9a1c10a21d58282eea
AhmadElHalabi10 <ahmd.el.halabi.4@gmail.com>
2023-06-04 16:54:47 +0300

    Changing the layout image source in the home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./home.css";
-import ProductCardPhoto1 from "../../uploads/ProductCardPhoto1.jpg";
-import ProductCardPhoto2 from "../../uploads/ProductCardPhoto2.jpg";
-import ProductCardPhoto3 from "../../uploads/ProductCardPhoto3.jpg";
 import CardPhoto1 from "../../uploads/CardPhoto1.jpg";
 import CardPhoto2 from "../../uploads/CardPhoto2.jpg";
 import CardPhoto3 from "../../uploads/CardPhoto3.jpg";
@@ -59,27 +56,12 @@ export default function Home() {
       </div>
       <div className="home-products">
         <div className="home-products-product1">
-          <img
-            className="home-products-img"
-            src={ProductCardPhoto1}
-            alt="Hello1"
-          />
           <div className="home-products-numberofproperties">3 properties</div>
         </div>
         <div className="home-products-product2">
-          <img
-            className="home-products-img"
-            src={ProductCardPhoto2}
-            alt="Hello2"
-          />
           <div className="home-products-numberofproperties">2 properties</div>
         </div>
         <div className="home-products-product3">
-          <img
-            className="home-products-img"
-            src={ProductCardPhoto3}
-            alt="Hello3"
-          />
           <div className="home-products-numberofproperties">1 properties</div>
         </div>
       </div>
@@ -96,7 +78,7 @@ export default function Home() {
         <div className="home-propertiesYouMayLike-products">
           <Card
             className="home-propertiesYouMayLike-products-card1"
-            image={CardPhoto1}
+            image="https://i.ibb.co/9Wpzfjm/Card-Photo1.webp"
             price="30000"
             bedNumber="4"
             space="125"
@@ -104,7 +86,7 @@ export default function Home() {
           />
           <Card
             className="home-propertiesYouMayLike-products-card2"
-            image={CardPhoto2}
+            image="https://i.ibb.co/jwG7xKr/Card-Photo2.webp"
             price="40000"
             bedNumber="3"
             space="135"
@@ -112,7 +94,7 @@ export default function Home() {
           />
           <Card
             className="home-propertiesYouMayLike-products-card2"
-            image={CardPhoto3}
+            image="https://i.ibb.co/9yWGK87/Card-Photo3.webp"
             price="50000"
             bedNumber="5"
             space="145"
